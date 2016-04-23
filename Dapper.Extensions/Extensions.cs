8b47c27158a21a0a8cccb55b5e6564f331ae8900
zhang-xiao-ming <website_net@163.com>
2016-04-23 12:47:27 +0800

    扩展DynamicParameters

--- Dapper.Extensions/Extensions.cs
+++ Dapper.Extensions/Extensions.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Data;
+using System.Xml.Linq;
 
 namespace Dapper.Extensions
 {
     public static class Extensions
     {
-
+        private static readonly DateTime DateTime19000101 = new DateTime(1900, 1, 1);
         #region IDataReader
         public static T GetValue<T>(this IDataReader dr, int i)
         {
@@ -46,6 +47,182 @@ namespace Dapper.Extensions
         }
         #endregion
 
+        #region DynamicParameters
 
+        public static void Add(this DynamicParameters dynamicParameters, string name, char? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.AnsiString);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, char value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.AnsiString);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters,string name,bool? value)
+        {
+            if(dynamicParameters==null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Boolean);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, bool value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Boolean);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, byte? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Int16);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, byte value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Int16);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, short? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Int16);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, short value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Int16);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, int? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Int32);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, int value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Int32);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, long? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Int64);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, long value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Int64);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, decimal? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Decimal);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, decimal value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Decimal);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, float? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Single);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, float value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Single);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, double? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Double);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, double value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Double);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, DateTime? value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value==null ||value.Value < DateTime19000101 ? DateTime19000101 : value.Value, DbType.DateTime);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, DateTime value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value < DateTime19000101 ? DateTime19000101 : value, DbType.DateTime);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, string value, int size, bool isUnicodeString = false, bool isLengthFixed = false)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, isUnicodeString ? (isLengthFixed ? DbType.StringFixedLength : DbType.String) : (isLengthFixed ? DbType.AnsiStringFixedLength : DbType.AnsiString), size: size);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, byte[] value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Binary);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, Guid value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Guid);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, XDocument value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Xml);
+        }
+
+        public static void Add(this DynamicParameters dynamicParameters, string name, DataTable value)
+        {
+            if (dynamicParameters == null)
+                return;
+            dynamicParameters.Add(name, value, DbType.Object);
+        }
+        #endregion
     }
 }
